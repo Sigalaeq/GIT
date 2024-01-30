bab0941042faad7676237f9391a44c174e94cebd
Esteban Sigala <13039@portalcedi.edu.mx>
2024-01-30 13:45:09 -0600

    cin ingresa tu edad

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,6 +5,16 @@ using namespace std;
 int main()
 {
     cout<<"Hola"<<endl;
+    int x;
+    cout<<"Ingresa tu edad "<<endl;
+cin>>x;
+if(x<18)
+{
+    cout<<"Eres menor de edad";
+}
+else{
+    cout<<"Eres mayor de edad";
+}
     return 0;
 }
 
